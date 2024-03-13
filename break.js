6e256f72d6c48708bc23cd5c5262078b8c25d938
Distance71 <agusdaverede@yahoo.com.ar>
2024-03-13 17:23:53 -0300

    Eslint & prettier

--- break.js
+++ break.js
@@ -1,34 +1,34 @@
-var utils = require('./utils')
+var utils = require("./utils");
 
 // break utxos into the maximum number of 'output' possible
-module.exports = function broken (utxos, output, feeRate) {
-  if (!isFinite(utils.uintOrNaN(feeRate))) return {}
+module.exports = function broken(utxos, output, feeRate) {
+  if (!isFinite(utils.uintOrNaN(feeRate))) return {};
 
-  var bytesAccum = utils.transactionBytes(utxos, [])
-  var value = utils.uintOrNaN(output.value)
-  var inAccum = utils.sumOrNaN(utxos)
-  if (!isFinite(value) ||
-      !isFinite(inAccum)) return { fee: feeRate * bytesAccum }
+  var bytesAccum = utils.transactionBytes(utxos, []);
+  var value = utils.uintOrNaN(output.value);
+  var inAccum = utils.sumOrNaN(utxos);
+  if (!isFinite(value) || !isFinite(inAccum))
+    return { fee: feeRate * bytesAccum };
 
-  var outputBytes = utils.outputBytes(output)
-  var outAccum = 0
-  var outputs = []
+  var outputBytes = utils.outputBytes(output);
+  var outAccum = 0;
+  var outputs = [];
 
   while (true) {
-    var fee = feeRate * (bytesAccum + outputBytes)
+    var fee = feeRate * (bytesAccum + outputBytes);
 
     // did we bust?
-    if (inAccum < (outAccum + fee + value)) {
+    if (inAccum < outAccum + fee + value) {
       // premature?
-      if (outAccum === 0) return { fee: fee }
+      if (outAccum === 0) return { fee: fee };
 
-      break
+      break;
     }
 
-    bytesAccum += outputBytes
-    outAccum += value
-    outputs.push(output)
+    bytesAccum += outputBytes;
+    outAccum += value;
+    outputs.push(output);
   }
 
-  return utils.finalize(utxos, outputs, feeRate)
-}
+  return utils.finalize(utxos, outputs, feeRate);
+};
